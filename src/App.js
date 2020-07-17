84f52b12478c5046bde8eb3313fb043bb4898786
luciano <luciano.fndz@gmail.com>
2020-07-16 22:36:22 -0300

    0004 - modified view

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import store from './store/store';
 import { Provider } from 'react-redux';
 import DashboardContainer from './containers/dashboardContainer';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
